5d8958d9963184f73d3f9a09ad8c0d747cf69a0c
Doyoon Kim <cheeezing_u@naver.com>
2017-11-22 21:42:29 +0900

    TDD 1일차

---
+++ src/main/java/Account.java
@@ -0,0 +1,12 @@
+public class Account {
+
+    private int balance;
+
+    public Account(int balance) {
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
